e6337ef7a5aeb828da144945ef50f36232841f3b
MobileDev0702 <mobiledev0702@hotmail.com>
2020-01-20 13:29:33 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Editor, EditorState, RichUtils } from 'draft-js';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+  }
+
+  onChange = (editorState) => {
+    this.setState({ editorState });
+  };
+
+  handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+
+    return 'not-handled';
+  }
+
+  onUnderlineClick = () => {
+    // this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+    // Send Action...
+    
+  }
+
+  onToggleCode = () => {
+    this.onChange(RichUtils.toggleCode(this.state.editorState));
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onUnderlineClick}>Send</button>
+        <button onClick={this.onToggleCode}>Code Block</button>
+        <Editor
+          editorState={this.state.editorState}
+          handleKeyCommand={this.handleKeyCommand}
+          onChange={this.onChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
