f3a3bf7d2fb9b7d5fad935deeff7158eae3b3205
MobileDev0702 <mobiledev0702@hotmail.com>
2020-01-21 23:28:40 +0800

    Updates

---
+++ precache-manifest.c7263d7c0137836902909c058a6bf0fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91cffcd33d501943d2c3",
+    "url": "/message/static/css/main.43192fd6.chunk.css"
+  },
+  {
+    "revision": "91cffcd33d501943d2c3",
+    "url": "/message/static/js/main.91cffcd3.chunk.js"
+  },
+  {
+    "revision": "c0d7e1b0e42cf79b102c",
+    "url": "/message/static/css/1.d44c6c97.chunk.css"
+  },
+  {
+    "revision": "c0d7e1b0e42cf79b102c",
+    "url": "/message/static/js/1.c0d7e1b0.chunk.js"
+  },
+  {
+    "revision": "3468d9e2993b3c2fba9e",
+    "url": "/message/static/js/runtime~main.3468d9e2.js"
+  },
+  {
+    "revision": "7178b8996ac6992c241bba3b9e29f726",
+    "url": "/message/index.html"
+  }
+];
