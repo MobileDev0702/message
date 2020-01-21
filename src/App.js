47140a8b709a37544c495c506d34be41a5ac3175
MobileDev0702 <mobiledev0702@hotmail.com>
2020-01-21 17:02:01 +0800

    integrate auth

--- src/App.js
+++ src/App.js
@@ -1,138 +1,105 @@
-import React, { Component } from 'react';
-import { Editor, EditorState, RichUtils } from 'draft-js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-class App extends Component {
+import React, { Component } from 'react'
+import io from 'socket.io-client'
+import FontAwesome from 'react-fontawesome'
+import './App.css'
+import Home from './Home'
+// import Home from './Home'
+const API_URL = 'http://127.0.0.1:8080'
+const socket = io(API_URL)
+
+export default class App extends Component {
+
   constructor() {
-    super();
+    super()
     this.state = {
-      editorState: EditorState.createEmpty(),
-    };
-  }
-
-  onChange = (editorState) => {
-    this.setState({ editorState });
-  };
-
-  handleKeyCommand = (command) => {
-    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
-
-    if (newState) {
-      this.onChange(newState);
-      return 'handled';
+      user: {},
+      disabled: ''
     }
-
-    return 'not-handled';
+    this.popup = null  
+    localStorage.setItem('isSigned', false)
   }
 
-  onUnderlineClick = () => {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+  componentDidMount() {
+    socket.on('user', user => {
+      this.popup.close()
+      this.setState({user})
+    })
   }
 
-  onToggleCode = () => {
-    this.onChange(RichUtils.toggleCode(this.state.editorState));
+  checkPopup() {
+    const check = setInterval(() => {
+      const { popup } = this
+      if (!popup || popup.closed || popup.closed === undefined) {
+        clearInterval(check)
+        this.setState({ disabled: ''})
+      }
+    }, 1000)
+  }
+
+  openPopup() {
+    const width = 600, height = 600
+    const left = (window.innerWidth / 2) - (width / 2)
+    const top = (window.innerHeight / 2) - (height / 2)
+    
+    const url = `${API_URL}/twitter?socketId=${socket.id}`
+
+    return window.open(url, '',       
+      `toolbar=no, location=no, directories=no, status=no, menubar=no, 
+      scrollbars=no, resizable=no, copyhistory=no, width=${width}, 
+      height=${height}, top=${top}, left=${left}`
+    )
+  }
+
+  startAuth() {
+    if (!this.state.disabled) {  
+      this.popup = this.openPopup()  
+      this.checkPopup()
+      this.setState({disabled: 'disabled'})
+    }
+  }
+
+  closeCard() {
+    this.setState({user: {}})
   }
 
   render() {
+    const { name } = this.state.user
+    // const { name, photo } = this.state.user
+    const { disabled } = this.state
+    
+    
+    name?localStorage.setItem("isSigned", true):localStorage.setItem('isSigned', false);
+    
+
     return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container">
-            <a className="navbar-brand" href="#">
-              <h1><i className="material-icons">home	</i> OutReach Assistant</h1>
-            </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <a className="nav-link" href="#"><h4>John Smith</h4></a>
-                </li>
-              </ul>
+      <div className={'container'}>
+        {/* If there is a user, show the user */}
+        {/* Otherwise show the login button */}
+        {name
+        // localStorage.getItem('isSigned')
+          // ? <div className={'card'}>              
+          //     <img src={photo} alt={name} />
+          //     <FontAwesome
+          //       name={'times-circle'}
+          //       className={'close'}
+          //       onClick={this.closeCard.bind(this)}
+          //     />
+          //     <h4>{`@${name}`}</h4>
+          //   </div> 
+          ? <Home user= {this.state.user}/>
+          : <div className={'button'}>
+              <button 
+                type='button'
+                className='btn btn-lg'
+                onClick={this.startAuth.bind(this)} 
+                style={{borderRadius: '50%'}}
+
+              ><img src={'./twitter.png'} style={{width:'200px'}} />
+              </button>
+              
             </div>
-          </div>
-      
-        </nav>
-        <div className="container">
-          <div className="card card-primary">
-            <div className="card-header">
-            
-              <i className="material-icons" style={{float:'right'}}>fullscreen_exit	</i>
-              <h2><i className="material-icons">library_books	</i> Campaigns</h2>
-            </div>
-            <div className="card-body" style={{backgroundColor:'gray'}}>
-              <div className="container" style={{backgroundColor:'#dcdcdc'}}>
-                <div className="row">
-                  <div className="col-xl-3"></div>
-                  <div className="col-xl-6">
-                    <h1>Build a Campaign Message</h1>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col col-xl-4">
-                      <div className="row">
-                        <div className="col-6">
-                          <div className='directory'>
-                            This is text1.
-                          </div>
-                        </div>
-                        <div className="col-6">
-                          <div className='directory'>
-                            This is text2.
-                          </div>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="col-6">
-                          <div className='directory'>
-                            This is text3.
-                          </div>
-                        </div>
-                        <div className="col-6">
-                          <div className='directory'>
-                            This is text4.
-                          </div>
-                        </div>
-                      </div>
-                  </div>
-                  <div className="col col-8">
-                    <div className="mPanel">
-                      <input className="form-control" type="text" placeholder="Subject"/>
-                      <input className="form-control" type="text" placeholder="Recipients"/>
-                      <div style={{height:'400px', padding:'2%'}}>
-                        +
-                      </div>
-                      <div className='row'>
-                        <div className='col-sm-3'>
-                          <button className="btn btn-primary bSend" onClick={this.onToggleCode}>Send</button>
-                        </div>
-                          
-                        <div className='col-sm-9'>
-                          <div className='row' style={{float:'right'}}>Word Count: 9999</div><br/>
-                          <div className='row' style={{float:'right'}}>Points of Personalization: 9999</div>
-                        </div>
-                      </div>                      
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="card-body">
-              <Editor
-                editorState={this.state.editorState}
-                handleKeyCommand={this.handleKeyCommand}
-                onChange={this.onChange}
-              />
-            </div>
-          </div>
-          
-          
-        </div>
+        }
       </div>
-      
-      
-    );
+    )
   }
 }
-
-export default App;
---
+++ src/Home.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import { Editor, EditorState, RichUtils } from 'draft-js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+  }
+
+  onChange = (editorState) => {
+    this.setState({ editorState });
+  };
+
+  handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+
+    return 'not-handled';
+  }
+
+  onUnderlineClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+  }
+
+  onToggleCode = () => {
+    this.onChange(RichUtils.toggleCode(this.state.editorState));
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container">
+            <a className="navbar-brand">
+              <h1><i className="material-icons">home	</i> OutReach Assistant</h1>
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <a className="nav-link"><h4>John Smith</h4></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+      
+        </nav>
+        <div className="container">
+          <div className="card card-primary">
+            <div className="card-header">
+            
+              <i className="material-icons" style={{float:'right'}}>fullscreen_exit	</i>
+              <h2><i className="material-icons">library_books	</i> Campaigns</h2>
+            </div>
+            <div className="card-body" style={{backgroundColor:'gray'}}>
+              <div className="container" style={{backgroundColor:'#dcdcdc'}}>
+                <div className="row">
+                  <div className="col-xl-3"></div>
+                  <div className="col-xl-6">
+                    <h1>Build a Campaign Message</h1>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col col-xl-4">
+                      <div className="row">
+                        <div className="col-6">
+                          <div className='directory'>
+                            This is text1.
+                          </div>
+                        </div>
+                        <div className="col-6">
+                          <div className='directory'>
+                            This is text2.
+                          </div>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-6">
+                          <div className='directory'>
+                            This is text3.
+                          </div>
+                        </div>
+                        <div className="col-6">
+                          <div className='directory'>
+                            This is text4.
+                          </div>
+                        </div>
+                      </div>
+                  </div>
+                  <div className="col col-8">
+                    <div className="mPanel">
+                      <input className="form-control" type="text" placeholder="Subject"/>
+                      <input className="form-control" type="text" placeholder="Recipients"/>
+                      <div style={{height:'400px', padding:'2%'}}>
+                        +
+                      </div>
+                      <div className='row'>
+                        <div className='col-sm-3'>
+                          <button className="btn btn-primary bSend" onClick={this.onToggleCode}>Send</button>
+                        </div>
+                          
+                        <div className='col-sm-9'>
+                          <div className='row' style={{float:'right'}}>Word Count: 9999</div><br/>
+                          <div className='row' style={{float:'right'}}>Points of Personalization: 9999</div>
+                        </div>
+                      </div>                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="card-body">
+              <Editor
+                editorState={this.state.editorState}
+                handleKeyCommand={this.handleKeyCommand}
+                onChange={this.onChange}
+              />
+            </div>
+          </div>
+          
+          
+        </div>
+      </div>
+      
+      
+    );
+  }
+}
+
+export default Home;
